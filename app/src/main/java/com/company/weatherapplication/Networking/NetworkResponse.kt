67ab1c3f638bb7342b1a0f74646183d128ada0b9
Abdullah Al Abid <97254210+abidvai@users.noreply.github.com>
2024-06-02 21:25:50 +0600

    a

--- app/src/main/java/com/company/weatherapplication/Networking/NetworkResponse.kt
+++ app/src/main/java/com/company/weatherapplication/Networking/NetworkResponse.kt
@@ -3,9 +3,12 @@ package com.company.weatherapplication.Networking
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
 
 sealed class NetworkResponse<T>(val data: T? = null,
-    message: String? = null) {
+    message: String? = null
+) {
 
     class Success<T>(data: T?) : NetworkResponse<T>(data = data)
     class Loading<T>(message: String): NetworkResponse<T>()
     class Error<T>(message: String?) : NetworkResponse<T>(message = message)
+
+
 }
--- app/src/main/java/com/company/weatherapplication/Screens/HomePage.kt
+++ app/src/main/java/com/company/weatherapplication/Screens/HomePage.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.company.weatherapplication.Networking.NetworkResponse
 import com.company.weatherapplication.R
