ea193df81e90c6154106d21278b8e6bc7b622333
Abdullah Al Abid <97254210+abidvai@users.noreply.github.com>
2024-06-02 21:30:42 +0600

    commit

--- app/src/main/java/com/company/weatherapplication/Networking/NetworkResponse.kt
+++ app/src/main/java/com/company/weatherapplication/Networking/NetworkResponse.kt
@@ -3,7 +3,7 @@ package com.company.weatherapplication.Networking
 import androidx.core.app.NotificationCompat.MessagingStyle.Message
 
 sealed class NetworkResponse<T>(val data: T? = null,
-    message: String? = null
+    val message: String? = null
 ) {
 
     class Success<T>(data: T?) : NetworkResponse<T>(data = data)
